b33c0ec5f3442d7cde4f903f0d02dfd4d2f3d7db
Nyla Upal <nylaupal@gmail.com>
2026-03-23 04:04:34 +0000

    update files

--- demographics.py
+++ demographics.py
@@ -7,6 +7,27 @@ df = pd.read_csv("Neighorhood_Indicators_CoreDataDownload_2025-05-15.csv")
 neighborhood = df.loc[:,['region_name', 'region_type', 'year', 'crime_viol_rt', 'hh_inc_med_adj', 
     'hh_u18_pct', 'pop_65p_pct', 'pop_num']]
 
+#only take recent demographics
 neighborhood = neighborhood[neighborhood['year']=='2023']
+
+#drop unneccesary characters 
+neighborhood["hh_inc_med_adj"] = (
+    neighborhood["hh_inc_med_adj"]
+    .str.replace("$", "", regex=False)
+    .str.replace(",", "", regex=False)
+    .astype(float)
+)
+
+neighborhood["hh_u18_pct"] = (
+    neighborhood["hh_u18_pct"]
+    .str.replace("%", "", regex=False)
+    .astype(float)
+)
+
+neighborhood["pop_65p_pct"] = (
+    neighborhood["pop_65p_pct"]
+    .str.replace("%", "", regex=False)
+    .astype(float)
+)
 
 neighborhood.to_csv('Demographics.csv')
